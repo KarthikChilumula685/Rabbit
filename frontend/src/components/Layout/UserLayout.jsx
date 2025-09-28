1cccaf702b442974709df8438d0deb56051089ce
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-09-29 00:20:26 +0530

    first commit

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../Common/Header'
+import Navbar from '../Common/Navbar'
+import Footer from '../Common/Footer'
+import { Outlet } from 'react-router-dom'
+
+const UserLayout = () => {
+  return (
+    <>
+      {/* Herder */}
+      <Header/>
+      {/* Main contenet */}
+      <main>
+        <Outlet/>
+      </main>
+      {/* Footer */}
+      <Footer/>
+    </>
+  )
+}
+
+export default UserLayout
