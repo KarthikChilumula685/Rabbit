315ca4f6671fbedeab8573a8fc9739ada7b9aa54
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-09-29 20:49:05 +0530

    feat: enhance NewArrivals component with scroll functionality and image links

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const NewArrivals = () => {
+  const scrollRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+
   const newArrivals = [
     {
       _id: "1",
@@ -9,7 +17,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=1",
+          url: "https://picsum.photos/500/500?random=1",
           altText: "Stylish Jacket",
         },
       ],
@@ -20,7 +28,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=2",
+          url: "https://picsum.photos/500/500?random=2",
           altText: "Stylish Jacket",
         },
       ],
@@ -31,7 +39,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=3",
+          url: "https://picsum.photos/500/500?random=3",
           altText: "Stylish Jacket",
         },
       ],
@@ -42,7 +50,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=4",
+          url: "https://picsum.photos/500/500?random=4",
           altText: "Stylish Jacket",
         },
       ],
@@ -53,7 +61,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=5",
+          url: "https://picsum.photos/500/500?random=5",
           altText: "Stylish Jacket",
         },
       ],
@@ -64,7 +72,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=6",
+          url: "https://picsum.photos/500/500?random=6",
           altText: "Stylish Jacket",
         },
       ],
@@ -75,7 +83,7 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=7",
+          url: "https://picsum.photos/500/500?random=7",
           altText: "Stylish Jacket",
         },
       ],
@@ -86,15 +94,64 @@ const NewArrivals = () => {
       price: 120,
       image: [
         {
-          url: "https://picsum.photoes/500/500?random=8",
+          url: "https://picsum.photos/500/500?random=8",
           altText: "Stylish Jacket",
         },
       ],
     },
   ];
 
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = x - startX;
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const handleMouseUpOrLeave = () => {};
+
+  const scroll = (direction) => {
+    const scrollAmount = direction === "left" ? -300 : 300;
+    scrollRef.current.scrollBy({ left: scrollAmount, behaviour: "smooth" });
+  };
+
+  //update scroll buttons
+  const updateScrollButtons = () => {
+    const container = scrollRef.current;
+
+    if (container) {
+      const leftScroll = container.scrollLeft;
+      const righScrollable =
+        container.scrollWidth > leftScroll + container.clientWidth;
+
+      setCanScrollRight(righScrollable);
+      setCanScrollLeft(leftScroll > 0);
+    }
+    console.log({
+      scrollLeft: container.scrollLeft,
+      clientWidth: container.clientWidth,
+      containerScrollWidth: container.scrollWidth,
+      offsetLeft: container.offsetLeft,
+    });
+  };
+
+  useEffect(() => {
+    const container = scrollRef.current;
+    if (container) {
+      container.addEventListener("scroll", updateScrollButtons);
+      updateScrollButtons();
+      return () => container.removeEventListener("scroll", updateScrollButtons);
+    }
+  },[]);
+
   return (
-    <section>
+    <section className="py-16 px-4 lg:px-0">
       <div className="container mx-auto text-center mb-10 relative">
         <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
         <p className="text-lg text-gray-600 mb-8">
@@ -104,23 +161,58 @@ const NewArrivals = () => {
 
         {/* Scroll buttons */}
         <div className="absolute right-0 bottom-[-30px] flex space-x-2">
-          <button className="p-2 rounded border bg-white text-black">
+          <button
+            onClick={() => scroll("left")}
+            disabled={!canScrollLeft}
+            className={`p-2 rounded border ${
+              canScrollLeft
+                ? "bg-white text-black"
+                : "bg-gray-200 text-gray-400 cursor-not-allowed"
+            }`}
+          >
             <FiChevronLeft className="text-2xl" />
           </button>
-          <button className="p-2 rounded border bg-white text-black">
+          <button
+            onClick={() => scroll("right")}
+            className={`p-2 rounded border ${
+              canScrollRight
+                ? "bg-white text-black"
+                : "bg-gray-200 text-gray-400 cursor-not-allowed"
+            }`}
+          >
             <FiChevronRight className="text-2xl" />
           </button>
         </div>
       </div>
 
       {/* Scrollable Conten? */}
-      <div className="container mx-auto overflow-x-scroll flex space-x-6 relative">
+      <div
+        ref={scrollRef}
+        className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${
+          isDragging ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
+      >
         {newArrivals.map((product) => (
-          <div key={product._id}>
+          <div
+            key={product._id}
+            className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative"
+          >
             <img
               src={product.image[0]?.url}
               alt={product.image[0]?.altText || product.name}
+              className="w-full h-[500px] object-cover rounded-lg"
+              draggable="false"
             />
+            <div className="absolute bottom-0 left-0 right-0 backdrop-blur-md text-white p-4 rounded-b-lg">
+              <Link to={`/products/${product._id}`} className="block">
+                <h4 className="font-medium">{product.name}</h4>
+                <p className="mt-1">${product.price} </p>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
