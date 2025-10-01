e73311fc23f3b76e2f47b94338d7f498a625641d
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-10-01 14:40:22 +0530

    edited home page

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const selectedProduct = {
   name: "Stylish Jacket",
@@ -22,6 +23,24 @@ const selectedProduct = {
 };
 
 const ProductDetails = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  const increase = () => {
+    setQuantity((prev) => prev + 1);
+  };
+  const decrease = () => {
+    if (quantity != 0) {
+      setQuantity((prev) => prev - 1);
+    }
+  };
+
+  const [mainImage, setMainImage] = useState("");
+  useEffect(() => {
+    if (selectedProduct?.images?.length > 0) {
+      setMainImage(selectedProduct.images[0].url);
+    }
+  }, [selectedProduct]);
+
   return (
     <div className="p-6">
       <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
@@ -33,7 +52,10 @@ const ProductDetails = () => {
                 key={index}
                 src={image.url}
                 alt={image.altText || `Thumbnail ${index}`}
-                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                  mainImage === image.url ? "border-black" : "border-gray-300"
+                }`}
+                onClick={() => setMainImage(image.url)}
               />
             ))}
           </div>
@@ -41,7 +63,7 @@ const ProductDetails = () => {
           <div className="md:w-1/2">
             <div className="mb-4">
               <img
-                src={selectedProduct.images[0]?.url}
+                src={mainImage}
                 alt="Main Product"
                 className="w-full h-auto object-cover rounded-lg"
               />
@@ -54,13 +76,16 @@ const ProductDetails = () => {
                 key={index}
                 src={image.url}
                 alt={image.altText || `Thumbnail ${index}`}
-                className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                  mainImage === image.url ? "border-black" : "border-gray-300"
+                }`}
+                onClick={() => setMainImage(image.url)}
               />
             ))}
           </div>
 
           {/* Right section */}
-          <div className="md:w-1/2 md:mml-10">
+          <div className="md:w-1/2 md:ml-10">
             <h1 className="text-2xl md:text-3xl font-semibold mb-2">
               {selectedProduct.name}
             </h1>
@@ -74,20 +99,71 @@ const ProductDetails = () => {
             <p className="text-gray-600 mb-4">{selectedProduct.description}</p>
 
             <div className="mb-4">
-              <p className="text-gray-700">
-                <div className="flex gap-2 mt-2">
-                  {selectedProduct.colors.map((color) => (
-                    <button
-                      key={color}
-                      className="w-8 h-8 rounded-full border"
-                      style={{
-                        backgroundColor: color.toLocaleLowerCase(),
-                        filter: "brightness(0.5)",
-                      }}
-                    ></button>
-                  ))}
-                </div>
-              </p>
+              <p className="text-gray-700">Color: </p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.colors.map((color) => (
+                  <button
+                    key={color}
+                    className="w-8 h-8 rounded-full border"
+                    style={{
+                      backgroundColor: color.toLocaleLowerCase(),
+                      filter: "brightness(0.5)",
+                    }}
+                  ></button>
+                ))}
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <p className="text-gray-700">Size:</p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.sizes.map((size) => (
+                  <button key={size} className="px-4 py-2 rounded border">
+                    {size}
+                  </button>
+                ))}
+              </div>
+            </div>
+
+            {/* Quantity */}
+            <div className="mb-6">
+              <p className="text-gray-700">Quantity:</p>
+              <div className="flex items-center space-x-4 mt-2">
+                <button
+                  onClick={decrease}
+                  className="px-2 py-1 bg-gray-200 rounded text-lg"
+                >
+                  -
+                </button>
+                <span className="text-lg">{quantity}</span>
+                <button
+                  onClick={increase}
+                  className="px-2 py-1 bg-gray-200 rounded text-lg"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+
+            {/* Add to cart */}
+            <button className="bg-black text-white py-2 px-6 rounded w-full mb-4">
+              ADD TO CART
+            </button>
+
+            <div className="mt-10 text-gray-700">
+              <h3 className="text-xl font-bold mb-4">Characteristics: </h3>
+              <table className="w-full text-left text-sm text-gray-600">
+                <tbody>
+                  <tr>
+                    <td className="py-1">Brand</td>
+                    <td className="py-1">{selectedProduct.brand}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Material</td>
+                    <td className="py-1">{selectedProduct.material}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
