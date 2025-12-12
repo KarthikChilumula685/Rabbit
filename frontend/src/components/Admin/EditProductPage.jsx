2713f2303863b151e01e2b012362d7194cc238bb
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-12-13 00:20:03 +0530

    feat: add OrderManagement component and integrate it into routing; enhance EditProductPage with additional fields and image upload functionality

--- frontend/src/components/Admin/EditProductPage.jsx
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -17,10 +17,12 @@ const EditProductPage = () => {
     gender: "",
     images: [
       {
-        url: "https://picsum.photos.150?random=1",
+        url: "https://picsum.photos/150?random=1",
+        altText: "1st image",
       },
       {
-        url: "https://picsum.photos.150?random=2",
+        url: "https://picsum.photos/150?random=2",
+        altText: "2nd image",
       },
     ],
   });
@@ -29,10 +31,22 @@ const EditProductPage = () => {
     const { name, value } = e.target;
     setProductData((prevData) => ({ ...prevData, [name]: value }));
   };
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(productData);
+    
+  }
+
   return (
     <div className="max-w-5xl mx-auto p-6 shadow-md rounded-md">
       <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* Name */}
         <div className="mb-6">
           <label className="block font-semibold mb-2">Product Name</label>
@@ -52,6 +66,7 @@ const EditProductPage = () => {
           <textarea
             name="description"
             value={productData.description}
+            onChange={handleChange}
             className="w-full border border-gray-300 rounded-md p-2"
             rows={4}
             required
@@ -69,6 +84,92 @@ const EditProductPage = () => {
             className="w-full border border-gray-300 rounded-md p-2"
           />
         </div>
+
+        {/* Count In stock */}
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">Count in Stock</label>
+          <input
+            type="number"
+            name="countInStock"
+            value={productData.countInStock}
+            onChange={handleChange}
+            className="w-full border border-gray-300 rounded-md p-2"
+          />
+        </div>
+
+        {/* SKU */}
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">SKU</label>
+          <input
+            type="text"
+            name="sku"
+            value={productData.sku}
+            onChange={handleChange}
+            className="w-full border border-gray-300 rounded-md p-2"
+          />
+        </div>
+
+        {/* sizes */}
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">
+            Sizes (comma-separated)
+          </label>
+          <input
+            type="text"
+            name="sizes"
+            value={productData.sizes.join(", ")}
+            onChange={(e) =>
+              setProductData({
+                ...productData,
+                sizes: e.target.value.split(",").map((size) => size.trim()),
+              })
+            }
+            className="w-full border border-gray-300 rounded-md p-2"
+          />
+        </div>
+
+        {/* Colors */}
+        <div className="mb-6">
+          <label className="block font-semibold mb-2">
+            Colors (comma-separated)
+          </label>
+          <input
+            type="text"
+            name="colors"
+            value={productData.colors.join(", ")}
+            onChange={(e) =>
+              setProductData({
+                ...productData,
+                colors: e.target.value.split(",").map((color) => color.trim()),
+              })
+            }
+            className="w-full border border-gray-300 rounded-md p-2"
+          />
+        </div>
+
+        {/* image upload */}
+        <div className="mb-6">
+          <label className="block font-medium mb-2">Upload Image</label>
+          <input type="file" onChange={handleImageUpload} />
+          <div className="flex gap-4 mt-4">
+            {productData.images.map((image, index) => (
+              <div key={index}>
+                <img
+                  src={image.url}
+                  alt={image.altText || "Product Image"}
+                  className="w-20 h-20 object-cover rounded-md shadow-md"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 cursor-pointer transition-colors"
+        >
+          Update Product
+        </button>
       </form>
     </div>
   );
